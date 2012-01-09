ab2da1bfd0420e29afda7626f481f3ef014165d1
Freddie <freddieboi@gmail.com>
2012-01-09 02:39:33 +0100

    Initial commit

---
+++ awesomeness.js
@@ -0,0 +1,47 @@
+console.log("Use the Source, Luke");
+
+// Initialize the Spotify objects
+var sp = getSpotifyApi(1);
+var m = sp.require("sp://import/scripts/api/models");
+var v = sp.require("sp://import/scripts/api/views");
+
+// I tend to leave these uncommented for fast access to the API calls
+console.log(sp.core);						
+console.log(sp.trackPlayer);
+console.log(sp.social);
+
+
+// Handle URI arguments
+sp.core.addEventListener("argumentsChanged", handleArgs);
+function handleArgs() {
+	var args = sp.core.getArguments();
+	$("section").hide();	// Hide all sections
+	$("#"+args[0]).show();	// Show current section
+
+	// If there are multiple arguments, handle them accordingly
+	if(args[1]) {		
+		switch(args[0]) {
+			case "calculate":
+				// Do something?
+				break;
+		}
+	}
+}
+
+// Handle Drag 'n' Drop
+sp.core.addEventListener("linksChanged", handleLinks);
+function handleLinks() {
+	var links = sp.core.getLinks();
+	if(links.length) {
+		// Play the given item
+		sp.trackPlayer.playTrackFromUri(links[0],{ onSuccess: function() {} });
+	} 
+}
+
+$(function(){
+
+	// Run on application load
+	handleArgs();
+	handleLinks();
+
+});
